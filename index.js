fa335b4d40501f62da8d49860249d4d2ebba6493
Dreamer20 <gv4alex@gmail.com>
2018-04-07 15:42:35 +0300

    Test heroku servive

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .get('/', (req, res) => res.send('hello fucking world'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
